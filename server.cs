fb39a7af438b64dfdbe12386ebf6f530f9c3fe30
Port <portification@gmail.com>
2013-06-26 12:27:36 +0200

    Properly format add-on for usage as a game-mode.

--- server.cs
+++ server.cs
@@ -1,4 +1,10 @@
 $ZAMB::Path = filePath(expandFileName("./description.txt")) @ "/";
 
+if ($GameModeArg !$= $ZAMB::Path @ "gamemode.txt") {
+	// TODO: Provide minimal version of ZAMB in Custom game-mode for map building.
+	error("ERROR: GameMode_ZAMB cannot be used in custom game-modes.");
+	return;
+}
+
 exec( "./lib/ts-pathing.cs" );
 exec( "./src/sounds.cs" );
