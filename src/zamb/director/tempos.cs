66eb9af9f9c3620bc869f3caa83a222be5207230
Port <portification@gmail.com>
2013-07-02 12:19:43 +0200

    Further director work.

---
+++ src/zamb/director/tempos.cs
@@ -0,0 +1,11 @@
+function ZAMB_Director::tickMake(%this) {
+}
+
+function ZAMB_Director::tickKeep(%this) {
+}
+
+function ZAMB_Director::tickFade(%this) {
+}
+
+function ZAMB_Director::tickWait(%this) {
+}
--- src/zamb/main.cs
+++ src/zamb/main.cs
@@ -1,7 +1,7 @@
 exec("./package.cs");
 exec("./zombies.cs");
 
-exec("./director/tick.cs");
+exec("./director/main.cs");
 
 $ZAMB::TickRate = 100;
 $ZAMB::ThinkLimit = 10;
@@ -71,5 +71,52 @@ function ZAMB::tick(%this) {
 	}
 
 	%this.director.tick();
+
+	if ($Sim::Time - %this.lastDebugTime >= 0.2) {
+		%this.debug();
+	}
+
 	%this.tick = %this.schedule($ZAMB::TickRate, "tick");
+}
+
+function ZAMB::debug(%this) {
+	switch (%this.director.tempo) {
+		case 0: %tempo = "PEAK_MAKE";
+		case 1: %tempo = "PEAK_KEEP";
+		case 2: %tempo = "PEAK_FADE";
+		case 3: %tempo = "PEAK_WAIT";
+	}
+
+	if (%this.director.length !$= "") {
+		%time = mCeil(%this.director.length - ($Sim::Time - %this.director.start));
+		%tempo = %tempo SPC "(" @ %time @ "s)";
+	}
+
+	%color = gc(%this.director.intensity);
+	%intensity = mFloatLength(%this.director.intensity * 100, 0);
+
+	%str = "\c6" @ %tempo SPC "at <color:" @ %color @ ">" @ %intensity @ "%";
+	%str = %str @ "\n\c6n = " @ %this.zombies.getCount();
+
+	bottomPrintAll("<font:consolas:16>" @ %str @ "\n", 1, 5);
+}
+
+function gc(%v)
+{
+	%g="0123456789ABCDEF";
+
+	if(%v>=0.5)
+	{
+		%v=mfloatlength((1 - ((%v-0.5) / 0.5))*15, 0);
+		%c=getsubstr(%g,%v,1);
+		return "FF"@%c@%c@"00";
+	}
+	else
+	{
+		%v=mfloatlength((%v / 0.5)*15, 0);
+		%c=getsubstr(%g,%v,1);
+		return %c@%c@"FF00";
+	}
+
+	return "FFFFFF";
 }
